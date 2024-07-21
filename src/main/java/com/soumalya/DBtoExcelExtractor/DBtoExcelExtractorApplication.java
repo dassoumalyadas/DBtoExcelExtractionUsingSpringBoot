fce77e42bc726a55046f9af684ba15fe25adb44b
dassoumalyadas <dassoumalyadas@gmail.com>
2024-07-21 09:34:32 +0530

    First commit.

---
+++ src/main/java/com/soumalya/DBtoExcelExtractor/DBtoExcelExtractorApplication.java
@@ -0,0 +1,14 @@
+package com.soumalya.DBtoExcelExtractor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DBtoExcelExtractorApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+		/*SpringApplication.run(DBtoExcelExtractorApplication.class, args);*/
+	}
+
+}
---
+++ src/test/java/com/soumalya/DBtoExcelExtractor/DBtoExcelExtractorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soumalya.DBtoExcelExtractor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DBtoExcelExtractorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
